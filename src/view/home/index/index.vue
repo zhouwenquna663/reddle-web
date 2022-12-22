7a55bbcf8ea2ab1b10b099e9682b65cfefd695d1
zhouwenquna663 <34540101+zhouwenquna663@users.noreply.github.com>
2022-12-22 13:32:10 +0800

    行内样式

--- src/view/home/index/index.vue
+++ src/view/home/index/index.vue
@@ -1,5 +1,5 @@
 <template>
  <div class="about">
-  <h1>视频下载</h1>
+  <h1 style="background-color:black">视频下载</h1>
  </div>
 </template>
--- src/view/home/test/index.vue
+++ src/view/home/test/index.vue
@@ -1,5 +1,5 @@
 <template>
  <div class="about">
-  <h1>test2</h1>
+  <h1 style="background-color:black">test2</h1>
  </div>
 </template>
